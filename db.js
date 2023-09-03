e0e941bb3c85bc348f536518ef4ff4a7502305f4
sneelika <sayuruneelika@gmail.com>
2023-09-03 14:12:24 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,26 @@
+const users = [
+  {
+    email: "test@test.com",
+    password: "test",
+  },
+];
+
+const publicPosts = [
+  {
+    title: "Test 1 title",
+    content: "Test 1 Content",
+  },
+];
+
+const privatePosts = [
+  {
+    title: "Private post title",
+    content: "Private post content",
+  },
+];
+
+module.exports = {
+  users,
+  publicPosts,
+  privatePosts,
+};
